1044a5ae9358bda11cbe383a7f4889d7d3dc7994
Vivian Song <viviann.song006@gmail.com>
2025-09-24 01:39:43 -0400

    deleted unecessary components

--- src/components/windows-window.tsx
+++ src/components/windows-window.tsx
@@ -10,7 +10,7 @@ interface WindowsWindowProps {
   title: string
   children: ReactNode
   className?: string
-  defaultMinimized?: boolean
+  // defaultMinimized?: boolean
   onClose?: () => void
 }
 
